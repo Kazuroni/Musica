9032b07982efd358d80ae242b56e751418e53e5d
Kazuroni <sayajinbr30@gmail.com>
2021-08-14 19:24:34 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Musica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Shinzou Wo Sasageyo");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Kore ijou no jigoku wa nai darou to shinjitakatta");
+            Console.WriteLine("Saredo jinrui saiaku no hi wa itsumo tōtotsu ni");
+            Console.WriteLine("Tobira wo tataku oto wa taezu hidoku busahou de");
+            Console.WriteLine("Manekarezaru saiyaku no hi wa akumu no you ni");
+            Console.WriteLine("");
+            Console.WriteLine("Sugishi hi wo uragiru mono");
+            Console.WriteLine("yatsura wa kuchiku subeki teki da");
+            Console.WriteLine("Ano hi donna kao de hitomi de");
+            Console.WriteLine("oretachi wo mitsumeteita");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("Nani wo sutereba akuma wo mo shinogeru?");
+            Console.WriteLine("Inochi sae tamashii sae kesshite oshiku nado wa nai");
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Sasageyo! Sasageyo! Shinzo wo Sasageyo!");
+            Console.WriteLine("Subete no gisei wa ima, kono toki no tame ni");
+            Console.WriteLine("Sasageyo! Sasageyo! Shinzo wo Sasageyo!");
+            Console.WriteLine("Susumubeki mirai o sono-te de kiri hirake!");
+        }
+    }
+}
